ee434db928501ae174486776342c807af88f119e
savom <56549021+savom@users.noreply.github.com>
2024-11-02 11:41:33 +0900

    Update fav_proc.php

--- php_to_DB folder/fav_proc.php
+++ php_to_DB folder/fav_proc.php
@@ -9,7 +9,7 @@
     $movie_num = $_POST['movieNum'];
     $movie_title = $_POST['movieTitle'];
 
-    $conn = mysqli_connect("codingmaker.net:33060", "khs0624", "0624", "khs0624");
+    $conn = mysqli_connect(your_host.port, ID, PW, folder);
 
     $u_num_sql = "select user_num from movie_user where user_id='$userid'";
     $u_num_qry = mysqli_query($conn, $u_num_sql);
