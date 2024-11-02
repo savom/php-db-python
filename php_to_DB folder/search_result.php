55db6b54427a88cbc9d82b2d56bc6f6329ed6826
savom <56549021+savom@users.noreply.github.com>
2024-11-02 11:45:46 +0900

    Update search_result.php

--- php_to_DB folder/search_result.php
+++ php_to_DB folder/search_result.php
@@ -51,7 +51,7 @@
 		</div>
 	</header>
 	<?php
-		$conn = mysqli_connect("codingmaker.net:33060", "khs0624", "0624", "khs0624");
+		$conn = mysqli_connect(your_host.port, ID, PW, folder);
         $sql = "select movie_num, movie_title, movie_poster from movie where movie_num=$search_movie_num";
         $qry = mysqli_query($conn, $sql);
 
